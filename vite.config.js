511eeca46b0fb8c2a0c830a496f22af073da66c1
adan55ted <adanzarate0703@gmail.com>
2025-03-02 16:25:49 -0600

    Actualización de archivos con los últimos cambios

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'/my-pagina-web/',
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
 })
